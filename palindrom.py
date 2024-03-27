e5c5f4cd1d4cb599fe6eab5b82043a0dc3a81e8e
Kamil Toporek <kamil.toporek98@gmail.com>
2024-03-27 13:38:37 +0100

    Adding function to verify if a word is a palindrome

--- palindrom.py
+++ palindrom.py
@@ -1,2 +1,7 @@
-def palindrom():
-    
+def palindrom(text):
+  length = len(text)
+  for i in range(0, length // 2):
+      if (text[i] != text[length - i - 1]):
+        return False
+      return True
+  
