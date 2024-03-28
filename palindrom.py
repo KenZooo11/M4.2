28404ac7820d033b81cfb6055b4988b956e9a166
KenZooo11 <164327728+KenZooo11@users.noreply.github.com>
2024-03-28 14:29:04 +0100

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,16 +1,16 @@
 def palindrom(text):
-  text = text.lower()
-  length = len(text)
-  for i in range(0, length // 2):
-      if (text[i] != text[length - i - 1]):
-        return False
-  return True
+    text = text.lower()
+    length = len(text)
+    for i in range(length // 2):
+        if text[i] != text[length - i - 1]:
+            return False
+    return True
   
   
-wyraz1 = "słoń"
-wyraz2 = "Kajak"
+wyraz1 = "Kajak"
+wyraz2 = "wędka"
 Wyraz3 = "adwokatka"
-wyraz4 = "klawiatura"
+wyraz4 = "słoń"
 
 print(palindrom(wyraz1))
 print(palindrom(wyraz2))
